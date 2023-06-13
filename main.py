ad603283f5d1b4d2cba79ecfa679adf5d436d0aa
Victor-Main <victorltrgh@gmail.com>
2023-06-13 18:29:45 -0300

    Início do pygame

---
+++ main.py
@@ -0,0 +1,30 @@
+import pygame
+
+WIDTH, HEIGHT = 900, 500
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Space Counter Battle!")
+
+WHITE = (255, 255, 255)
+
+
+def draw_window():
+    WIN.fill(WHITE)
+    pygame.display.update()
+
+
+def main():
+
+    run = True
+    while run:
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+        
+
+        draw_window()
+        
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()
