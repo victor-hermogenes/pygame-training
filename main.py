d2ff5955ae857e28c757039bc5440fd6fb4c80be
Victor-Main <victorltrgh@gmail.com>
2023-06-14 14:24:58 -0300

    Game finished

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 import pygame
 import os
+pygame.font.init()
+pygame.mixer.init()
 
 # Window Display
 WIDTH, HEIGHT = 900, 500
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Space Counter Battle!")
 BORDER = pygame.Rect(WIDTH//2 - 5, 0, 10, HEIGHT)
+
+#Sound effects
+BULLET_HIT_SOUND = pygame.mixer.Sound(os.path.join('Assets/Grenade+1.mp3'))
+BULLET_FIRE_SOUND = pygame.mixer.Sound(os.path.join('Assets', 'Gun+Silencer.mp3'))
+
+# Font to the health and winner text
+HEALTH_FONT = pygame.font.SysFont('comicsans', 40)
+WINNER_FONT = pygame.font.SysFont('comicsans', 100)
 
 # Window Colors
 WHITE = (255, 255, 255)
@@ -32,10 +42,16 @@ RED_SPACESHIP = pygame.transform.rotate(pygame.transform.scale(RED_SPACESHIP_IMA
 SPACE = pygame.transform.scale(pygame.image.load(os.path.join('Assets', 'space.png')), (WIDTH, HEIGHT))
 
 # Function to create Display
-def draw_window(red, yellow, red_bullets, yellow_bullets):
+def draw_window(red, yellow, red_bullets, yellow_bullets, red_health, yellow_health):
     """Function will get variables from above and draw the display"""
     WIN.blit(SPACE, (0, 0))
     pygame.draw.rect(WIN, BLACK, BORDER)
+
+    red_health_text = HEALTH_FONT.render("Health: " + str(red_health), 1, WHITE)
+    yellow_health_text = HEALTH_FONT.render("Health: " + str(yellow_health), 1, WHITE)
+    WIN.blit(red_health_text, (WIDTH - red_health_text.get_width() - 10, 10))
+    WIN.blit(yellow_health_text, (10, 10))
+
     WIN.blit(YELLOW_SPACESHIP, (yellow.x, yellow.y))
     WIN.blit(RED_SPACESHIP, (red.x, red.y))
 
@@ -71,6 +87,7 @@ def red_handle_movement(keys_pressed, red):
         elif keys_pressed[pygame.K_DOWN] and red.y + VEL + red.height < HEIGHT - 15: # DOWN
             red.y += VEL
 
+
 # Function to create bullets mechanics
 def handle_bullets(yellow_bullets, red_bullets, yellow, red):
      """Function will define how bullets are fired and collision efects as well"""
@@ -90,6 +107,15 @@ def handle_bullets(yellow_bullets, red_bullets, yellow, red):
           elif bullet.x < 0:
                red_bullets.remove(bullet)
 
+
+# Function to say that the game has a winner
+def draw_winner(text):
+     draw_text = WINNER_FONT.render(text, 1, WHITE)
+     WIN.blit(draw_text, (WIDTH/2 - draw_text.get_width()/2, HEIGHT/2 - draw_text.get_height()/2))
+     pygame.display.update()
+     pygame.time.delay(5000)
+
+
 # Function to define main game mechanics
 def main():
     """This function will define main games rules"""
@@ -99,6 +125,9 @@ def main():
     red_bullets = []
     yellow_bullets = []
 
+    red_health = 10
+    yellow_health = 10
+
     clock = pygame.time.Clock()
     run = True
     while run:
@@ -106,15 +135,37 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+                pygame.quit()
 
             if event.type == pygame.KEYDOWN:
                  if event.key == pygame.K_LCTRL and len(yellow_bullets) < MAX_BULLETS:
                       bullet = pygame.Rect(yellow.x + yellow.width, yellow.y + yellow.height//2 + 5, 10, 5)
                       yellow_bullets.append(bullet)
+                      BULLET_FIRE_SOUND.play()
 
                  if event.key == pygame.K_RCTRL and len(red_bullets) < MAX_BULLETS:
                       bullet = pygame.Rect(red.x, red.y + red.height//2 + 5, 10, 5)
                       red_bullets.append(bullet)
+                      BULLET_FIRE_SOUND.play()
+
+            if event.type == RED_HIT:
+                 red_health -= 1
+                 BULLET_HIT_SOUND.play()
+                 
+            if event.type == YELLOW_HIT:
+                 yellow_health -= 1
+                 BULLET_HIT_SOUND.play()
+
+        winner_text = ""
+        if red_health <= 0:
+             winner_text = "Yellow Wins!"
+
+        if yellow_health <= 0:
+             winner_text = "Red Wins!"
+
+        if winner_text != "":
+             draw_winner(winner_text)
+             break
 
         keys_pressed = pygame.key.get_pressed()
         yellow_handle_movement(keys_pressed, yellow)
@@ -122,9 +173,9 @@ def main():
 
         handle_bullets(yellow_bullets, red_bullets, yellow, red)
 
-        draw_window(red, yellow, red_bullets, yellow_bullets)
+        draw_window(red, yellow, red_bullets, yellow_bullets, red_health, yellow_health)
         
-    pygame.quit()
+    main()
 
 if __name__ == "__main__":
     main()
